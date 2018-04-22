ea888024ef61cf2966131716614602d37f1608eb
LKWLaLa <lkwlala@gmail.com>
2018-04-21 21:21:13 -0400

    add Url and Urls Container components

---
+++ src/components/Url.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Url = (props) => {  
+  return(
+    <div className="url-item">
+      <p>short url | long Url</p>
+    </div>
+  )
+}
+
+export default Url;
---
+++ src/components/UrlsContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Url from './Url'
+
+class UrlsContainer extends Component {
+  render() {
+    return (
+      <div>
+        "Urls Container"
+        <Input />
+        <Result />
+      </div>
+    );
+  }
+}
+
+export default UrlsContainer;
