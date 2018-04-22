c60765861c1d4b957f49916cb3df46d66be4f428
LKWLaLa <lkwlala@gmail.com>
2018-04-22 17:01:16 -0400

    add fetch request to top-100 and map results

--- src/components/UrlsContainer.js
+++ src/components/UrlsContainer.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react'
 import Url from './Url'
 
 class UrlsContainer extends Component {
+
+  constructor(){
+    super();
+    this.state = { urls: []}
+  }
+
+  ComponentDidMount(){
+    fetch('https://shrink-me.herokuapp.com/top-100').then(res => res.json())
+    .then(res => {
+      this.setState({urls: res})
+    })
+  }
+
+  mapUrls = () => {
+    this.state.urls.map((url) => <Url data={url} />)
+  }
+
   render() {
     return (
       <div>
-        "Urls Container"
-        <Input />
-        <Result />
+        {this.mapUrls()}
       </div>
     );
   }
